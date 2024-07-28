a96b30118592945310edb3dcdf3cb4dc8a579937
Agaxel1 <gabrielvillamar128@gmail.com>
2024-07-28 12:50:14 -0500

    Arreglando

--- src/discordClient.js
+++ src/discordClient.js
@@ -1,7 +1,7 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 const DISCORD_TOKEN = 'OTkyNDYxOTY3MTQ5MjUyNzA4.GKC7FI.hDGVi4Na4ni_gbyM5ZjNOhw1CrQvMUPZZ7aPOU';
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
 let isReady = false;
 
