164dbe59cfe93195326a8f33f74b89283ebc4c90
Agaxel1 <gabrielvillamar128@gmail.com>
2024-07-28 11:59:11 -0500

    Arreglando

--- src/modulos/anuncios/controlador.js
+++ src/modulos/anuncios/controlador.js
@@ -1,10 +1,15 @@
 const db = require('../../DB/mysql');
 const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js');
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
 const DISCORD_TOKEN = 'OTkyNDYxOTY3MTQ5MjUyNzA4.GKC7FI.hDGVi4Na4ni_gbyM5ZjNOhw1CrQvMUPZZ7aPOU';
 const CHANNEL_ID = '1267127952496132118';
 
-client.login(DISCORD_TOKEN);
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
+
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.login(DISCORD_TOKEN).catch(console.error);
 
 const TABLA = 'anunciosydeep';
 
