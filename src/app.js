6cdb19b55f500cec6d17651a35590dfd3fd2373a
Agaxel1 <gabrielvillamar128@gmail.com>
2024-08-14 23:09:26 -0500

    Correcion servidor

--- src/app.js
+++ src/app.js
@@ -21,7 +21,8 @@ const corsOptions = {
     origin: function (origin, callback) {
         const allowedOrigins = [
             'http://127.0.0.1:5500',
-            'https://api.vida-roleplay.com'
+            'https://api.vida-roleplay.com',
+            'https://vida-roleplay.com'
         ];
         if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
             callback(null, true);
